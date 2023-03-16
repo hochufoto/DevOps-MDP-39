8044b3e1d5401933492f7c8380c7d9900d777ddf
hochufoto <zalim@znotes.ru>
2023-03-16 20:43:13 +0300

    add comments

---
+++ Python/03_personal-info.py
@@ -0,0 +1,14 @@
+# Вводим имя
+firstName = input("What is your first name? ")
+# Вводим фамилию
+lastName = input("What is your last name? ")
+# Вводим город
+location = input("What is you location? ")
+# Вводим возраст
+age = input("What is your age? ")
+# Пробел
+space = " "
+
+# Выводим результат
+print("Hi"+space+firstName+space+lastName+"!"+space+"Your location is"+space+
+      location+space+"and you are"+space+age+space+"years old.")
