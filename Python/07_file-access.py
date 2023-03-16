ff5df31cb96d48d0977a54f55fa30f30c58104b4
hochufoto <zalim@znotes.ru>
2023-03-16 22:00:42 +0300

    Copy File Content Into a List Variable

--- Python/07_file-access.py
+++ Python/07_file-access.py
@@ -1,10 +1,14 @@
+# Создаем пустой список devices
+devices=[]
 # Создаем переменную содержимое которого читаем из файла devices.txt
 file = open("./Python/devices.txt","r")
 # с помощью for проходимся по всему файлу и заносим содержимое file в служебную переменную item
 for item in file:
 # удаляем пустые абзацы
     item=item.strip()
-# Выводим содержимое item
-    print(item)
+# добавляем элементы в список devices
+    devices.append(item)
 # Закрываем объязательно файл!!!
 file.close()
+# Выводим содержимое списка devices
+print(devices)
