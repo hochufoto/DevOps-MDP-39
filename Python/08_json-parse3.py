410c3ac7e587e4b8e5ac22a602abceccb5a61b12
hochufoto <zalim@znotes.ru>
2023-03-23 20:07:55 +0300

    Add User Input for Address

--- Python/08_json-parse3.py
+++ Python/08_json-parse3.py
@@ -11,24 +11,28 @@ import requests
 # dest куда едем
 # key api key
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
-orig = "Вашингтон"
-dest = "Москва"
 key = "aDuDw1nPuPKZPXgyZIeqa1gPZh7x5mM3"
+
+while True:
+    orig = input("Начальная точка: ")
+    dest = input("Пункт назначения: ")
+
+
 # Собираем ссылку
-url = main_api + urllib.parse.urlencode({
-    "key": key,
-    "from": orig,
-    "to": dest
-    })
+    url = main_api + urllib.parse.urlencode({
+        "key": key,
+        "from": orig,
+        "to": dest
+        })
 
 # Выводим url для браузера
-print("\n" + "URL: " + url + "\n")
+    print("\n" + "URL: " + url + "\n")
 
 # Переменная куда запишем ответ запроса в формате json
-json_data = requests.get(url).json()
+    json_data = requests.get(url).json()
 
-json_status = json_data["info"]["statuscode"]
-if json_status == 0:
-    print("API status: " + str(json_status) + " = A succesful route call. \n")
-else:
-    print("API status messages" + str(json_data["info"]["messages"]) + "\n")
+    json_status = json_data["info"]["statuscode"]
+    if json_status == 0:
+        print("API status: " + str(json_status) + " = A succesful route call. \n")
+    else:
+        print("API status messages" + str(json_data["info"]["messages"]) + "\n")
